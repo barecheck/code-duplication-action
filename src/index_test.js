a51365fd74c1a8bf075e8952cc6d6e0e41b3c1e0
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-07-16 18:48:15 +0100

    fix: change to commit

--- src/index_test.js
+++ src/index_test.js
@@ -4,7 +4,7 @@ const { duplicates } = require("@barecheck/scanner");
 async function main() {
   core.info("Hello from Github action");
 
-  const metrics = await duplicates.getMetrics("./src", "origin/master");
+  const metrics = await duplicates.getMetrics("./src", "644b0a1");
 
   // eslint-disable-next-line no-console
   console.log(metrics);

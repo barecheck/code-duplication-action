b3c16938c7d7783d0ec5201c5fee23538f75aa54
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-07-16 18:37:36 +0100

    fix: diff comparasion with commit

--- src/index_test.js
+++ src/index_test.js
@@ -4,7 +4,7 @@ const { duplicates } = require("@barecheck/scanner");
 async function main() {
   core.info("Hello from Github action");
 
-  const metrics = duplicates.getMetrics("./src", "master");
+  const metrics = duplicates.getMetrics("./src", "644b0a1");
 
   // eslint-disable-next-line no-console
   console.log(metrics);

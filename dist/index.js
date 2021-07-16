0d0cf120ce8a2629c14054443a49de55680cafa1
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-07-16 18:46:02 +0100

    build new version

--- dist/index.js
+++ dist/index.js
@@ -35973,7 +35973,7 @@ const { duplicates } = __nccwpck_require__(1728);
 async function main() {
   core.info("Hello from Github action");
 
-  const metrics = await duplicates.getMetrics("./src", "644b0a1");
+  const metrics = await duplicates.getMetrics("./src", "origin/master");
 
   // eslint-disable-next-line no-console
   console.log(metrics);
